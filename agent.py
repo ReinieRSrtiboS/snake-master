62180b3928a49c27c10b3c47179f0e62e69aec38
ReinieRSrtiboS <reinierstribos@gmail.com>
2018-01-19 00:44:23 +0100

    Hij doet shit

--- agent.py
+++ agent.py
@@ -1,29 +1,32 @@
 from queue import PriorityQueue
-from random import randint
 
 from gameobjects import GameObject
 from move import Move, Direction
 
 
 class Agent:
-    snakeHeadX = 0
-    snakeHeadY = 0
-    food = (None,None)
+    snakeHeadX = None
+    snakeHeadY = None
+    score = 0
+    food = (None, None)
 
     def get_move(self, board, score, turns_alive, turns_to_starve, direction):
 
-        for x in range (0,25):  #TODO maak variabel
-            for y in range (0,25):
+        if score is not self.score:
+            self.score = score
+
+        for x in range(0, 25):  # TODO make variable
+            for y in range(0, 25):
                 if board[x][y] == GameObject.SNAKE_HEAD:
                     self.snakeHeadX = x
                     self.snakeHeadY = y
                 if board[x][y] == GameObject.FOOD:
-                    self.food = (x,y)
+                    self.food = (x, y)
 
-        nextMove = None
+        next_move = None
         frontier = PriorityQueue()
         start = (self.snakeHeadX, self.snakeHeadY)
-        frontier.put(start, 0)
+        frontier.put(start, self.manhattan_distance(self.snakeHeadX, self.snakeHeadY))
         cost_so_far = {}
         came_from = {}
         cost_so_far[start] = 0
@@ -33,82 +36,73 @@ class Agent:
             current = frontier.get()
 
             if board[current[0]][current[1]] == GameObject.FOOD:
-                nextMove = current
+                next_move = current
 
                 path = ""
-                path += str(nextMove) + ", "
-                while came_from[nextMove] is not start:
-                    nextMove = came_from[nextMove]
-                    path += str(nextMove) + ", "
+                path += str(next_move) + ", "
+                while came_from[next_move] is not start:
+                    next_move = came_from[next_move]
+                    path += str(next_move) + ", "
                 break
 
-            for next in self.children(board, current[0], current[1]):
-                new_cost = cost_so_far[current] + 1
-                if (next not in cost_so_far or new_cost < cost_so_far[next]) and next[0] is not None:
-                    cost_so_far[next] = new_cost
-                    frontier.put(next, new_cost + self.manhattanDistance(next[0], next[1]))
-                    came_from[next] = current
+            if current is start:
+                for child in self.children(board, current[0], current[1], direction):
+                    new_cost = cost_so_far[current] + 1
+                    if child not in cost_so_far or new_cost < cost_so_far[child]:
+                        cost_so_far[child] = new_cost
+                        frontier.put(child, new_cost + self.manhattan_distance(child[0], child[1]))
+                        came_from[child] = current
 
-        # if nextMove is None:
-        #     moves = [Move.STRAIGHT, Move.LEFT, Move.RIGHT]
-        #     return moves[randint(0,2)]
-        # else:
-        #     print(nextMove)
+            else:
+                for child in self.children(board, current[0], current[1], self.get_new_direction(came_from[current][0], came_from[current][1], current[0], current[1])):
+                    new_cost = cost_so_far[current] + 1
+                    if child not in cost_so_far or new_cost < cost_so_far[child]:
+                        cost_so_far[child] = new_cost
+                        frontier.put(child, new_cost + self.manhattan_distance(child[0], child[1]))
+                        came_from[child] = current
 
-        return self.getDirection(direction, nextMove[0], nextMove[1])
+        # print(self.food)
+        # print(path)
+        return self.get_turn(direction, self.snakeHeadX, self.snakeHeadY, next_move[0], next_move[1])
 
-    def children(self, board, x, y):    #TODO maak variabel
-        available = [(None, None)] * 4
-        if x + 1 < 25 and (board[x + 1][y] == GameObject.EMPTY or board[x + 1][y] == GameObject.FOOD):
-            available[0] = (x + 1, y)
-        if x - 1 >= 0 and (board[x - 1][y] == GameObject.EMPTY or board[x - 1][y] == GameObject.FOOD):
-            available[1] = (x - 1, y)
-        if y + 1 < 25 and (board[x][y + 1] == GameObject.EMPTY or board[x][y + 1] == GameObject.FOOD):
-            available[2] = (x, y + 1)
-        if y - 1 >= 0 and (board[x][y - 1] == GameObject.EMPTY or board[x][y - 1] == GameObject.FOOD):
-            available[3] = (x, y - 1)
+    def children(self, board, x, y, direction):
+        available = []
+        for next in direction.get_xy_moves():
+            if 25 > x + next[0] >= 0 and 25 > y + next[1] >= 0:     # TODO make variable
+                if board[x + next[0]][y + next[1]] == GameObject.EMPTY or board[x + next[0]][y + next[1]] == GameObject.FOOD:
+                    available.append((x + next[0], y + next[1]))
+
         return available
 
-    def manhattanDistance(self, x, y):
+    def manhattan_distance(self, x, y):
         return abs(self.food[0] - x) + abs(self.food[1] - y)
 
-    def getDirection(self, direction, x, y):
-        difX = x - self.snakeHeadX
-        difY = y - self.snakeHeadY
+    def get_turn(self, direction, old_x, old_y, new_x, new_y):      # TODO check if it's not mirrored
+        dif_x = new_x - old_x
+        dif_y = new_y - old_y
 
-        if direction == Direction.NORTH and difY == -1:
-            return Move.STRAIGHT
-        if direction == Direction.NORTH and difX == 1:
-            return Move.RIGHT
-        if direction == Direction.NORTH and difX == -1:
-            return Move.LEFT
+        for i in range(0, 4):
+            if Direction(i).get_xy_manipulation() == (dif_x, dif_y):
+                new_direction = Direction(i)
 
-        if direction == Direction.EAST and difY == 1:
-            return Move.LEFT
-        if direction == Direction.EAST and difY == -1:
-            return Move.RIGHT
-        if direction == Direction.EAST and difX == 1:
-            return Move.STRAIGHT
+        return self.direction_to_move(direction, new_direction)
 
-        if direction == Direction.SOUTH and difY == 1:
-            return Move.STRAIGHT
-        if direction == Direction.SOUTH and difX == 1:
-            return Move.LEFT
-        if direction == Direction.SOUTH and difX == -1:
-            return Move.RIGHT
+    def direction_to_move(self, old_direction, new_direction):
+        for i in range(-1, 2):
+            if old_direction.get_new_direction(Move(i)) == new_direction:
+                return Move(i)
+        raise ValueError("Unable to get to new square")
 
-        if direction == Direction.WEST and difY == -1:
-            return Move.RIGHT
-        if direction == Direction.WEST and difY == 1:
-            return Move.LEFT
-        if direction == Direction.WEST and difX == -1:
-            return Move.STRAIGHT
+    def get_new_direction(self, old_x, old_y, new_x, new_y):
+        dif_x = new_x - old_x
+        dif_y = new_y - old_y
 
+        for i in range(0, 4):
+            if Direction(i).get_xy_manipulation() == (dif_x, dif_y):
+                new_direction = Direction(i)
+
+        return new_direction
 
     def on_die(self):
-        """This function will be called whenever the snake dies. After its dead the snake will be reincarnated into a
-        new snake and its life will start over. This means that the next time the get_move function is called,
-        it will be called for a fresh snake. Use this function to clean up variables specific to the life of a single
-        snake or to host a funeral.
-        """
+        raise ValueError("R.I.P.")
         pass
